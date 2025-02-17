b2a226e1dbc4db2a05550c81a9670df6263c3798
CodeshShubh <codeshshubh@gmail.com>
2025-02-17 09:03:04 +0530

    type_interface_optional_array_object_delartion

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,50 @@
 "use strict";
 // Data Types variable
-let c;
+// ->normal declaration
+let a;
 let b;
-const a = '1';
-console.log(a);
-// Data Type function
-const func = (a, b) => {
-    return a + b;
+let c;
+const d = 34; // const will must be initilized when we declare const other(let) can be only declare
+let e;
+//<-------------------------------------------------------------------------------------------------------------------------------------------->
+// ->array declaration
+let f; // also we can declare like this  let f:string[]
+let students = {
+    age: 1,
+    name: 'shubh',
+    gender: 'male'
 };
-const lab = 123;
+let teacher = {
+    age: 2,
+    name: 'Salman Khan',
+    gender: 'female',
+    certificate: false
+};
+// <-------------------------------------------------------------------------------------------------------------------------------------------->
+// -> we can give multiple datatypes to behave like normal js
+let a1;
+// let b1:string | number | Array<string> | boolean | { [key: string]: any } // -> but not declare array or object like this
+let b1;
+let f1;
+let students1 = {
+    age: 'one',
+    name: 'shubh',
+    gender: false
+};
+let teacher1 = {
+    age: 2,
+    name: 'Salman Khan',
+    gender: 'female',
+    certificate: false
+};
+const students2 = {
+    age: 1,
+    name: 'shubh',
+    gender: 'male'
+};
+const teacher2 = {
+    age: 2,
+    name: 'Karina Kapoor',
+    subject: 'Math',
+    certificate: true
+};
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,149 @@
 // Data Types variable
-let c:any;
-let b: string | number 
-const a:string ='1';
-console.log(a)
+// ->normal declaration
+let a:any;
+let b:string;
+let c:number;
+const d:number = 34; // const will must be initilized when we declare const other(let) can be only declare
+let e:boolean;
 
-// Data Type function
 
-const func =(a:number, b:number):number=>{
-    return a+b
+//<-------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+// ->array declaration
+let f:Array<string>; // also we can declare like this  let f:string[]
+
+
+
+
+//<-------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+// -> object declartion
+// let g:{age:number , name:string, certificate:boolean} = {
+//     age:1,
+//     name:'shubh',
+//     certificate:false
+// }
+
+// -> in above we declare object but this makes things bigger when we tring to make bigger object
+// --> so we use type alyesis
+type obj = {
+    age:number,
+    name:string,
+    certificate?:boolean, // by (?) we can make it optional it will not nessarery when we decalate object
+    gender:string
 }
 
-// or we can define 
-type Myfunc = number | string;
-const lab:Myfunc = 123;
+let students:obj = { // here we dont initilized certicate
+    age:1,
+    name:'shubh',
+    gender:'male'
+}
 
-type func2= (d: number, e: number) => number
+let teacher:obj = {
+    age:2,
+    name:'Salman Khan',
+    gender:'female',
+    certificate:false
+}
+
+
+
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+
+// -> we can give multiple datatypes to behave like normal js
+
+let a1:any;
+// let b1:string | number | Array<string> | boolean | { [key: string]: any } // -> but not declare array or object like this
+let b1:string | number | boolean
+
+let f1:Array<string | number | boolean>; 
+
+// -> type aylessis
+type obj1 = {
+    age:number | string,
+    name:string | number,
+    certificate?:boolean | number, // by (?) we can make it optional it will not nessarery when we decalate object
+    gender:string | boolean
+}
+
+let students1:obj1 = { // here we dont initilized certicate
+    age: 'one',
+    name:'shubh',
+    gender:false
+}
+
+let teacher1:obj = {
+    age:2,
+    name:'Salman Khan',
+    gender:'female',
+    certificate:false
+}
+
+
+
+
+
+// <-------------------------------------------------------------------------------------------------------------------------------------------->
+
+
+
+
+
+
+// -> interface ( by this we can inherti as a property and can add more property for other object using type)
+
+interface obj2 {
+    age:number,
+    name:string,
+    certificate:boolean,
+}
+
+type obj3 = {
+    age:number ,
+    name:string ,
+    certificate?:boolean , 
+}
+
+interface Myobj extends obj3 { // here we can inherit type using interface
+    gender:string
+}
+interface Myobj2 extends obj2 { // here we inherit interface using iterface
+    subject:string
+}
+
+
+const students2:Myobj = { // this is inherit  type  by using interface
+  age:1,
+  name:'shubh',
+  gender:'male'
+}
+
+const teacher2:Myobj2 = { // this is inherit interface using interface
+    age:2,
+    name:'Karina Kapoor',
+    subject:'Math',
+    certificate:true
+}
+
+
+
+
+
+
+
+
+
+
+
+
